733cc3e9ad8cbd735974d017ecd9b61b73b56c84
Bittu <bittukumarthakur@users.noreply.github.com>
2023-07-31 14:31:43 +0530

    greet user on connect

--- src/chat-app.js
+++ src/chat-app.js
@@ -2,17 +2,23 @@ const net = require("node:net");
 const PORT = 8000;
 
 const connectUser = (socket) => {
+  socket.setEncoding("utf-8");
+
   socket.write("Enter your name: ");
+  socket.once("data", (data) => {
+    const name = data.trim();
+    socket.write(`Hello ${name}!\n`);
+  });
 };
 
 const main = () => {
   const chatServer = net.createServer();
+
   chatServer.listen(PORT, () => {
     console.log("chat server is online");
   });
 
   chatServer.on("connection", (socket) => connectUser(socket));
-
 };
 
 main();
